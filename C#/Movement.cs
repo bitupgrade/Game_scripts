1a1e5668428fa30002a3701ed3c33170e5280ee5
Stefan <bitupgrade@outlook.com>
2020-04-08 23:46:31 +0100

    added C# folder with a movement script in it

---
+++ C#/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+    public float RunSpeed = 8;
+    public float speed = 4;
+    public float rotSpeed = 80;
+    public float rot = 0f;
+    public float gravity = 8;
+    public float NormalSpeed = 4;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+        anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        GetInput();
+    }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("walking", true);
+                if(anim.GetBool("attacking") == true)
+                {
+                    return;
+                }
+                else if (anim.GetBool("attacking") == false){
+                    anim.SetBool("walking", true);
+                    anim.SetInteger("condition", 1);
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir *= speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                
+            }
+            // Running
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("running", true);
+                anim.SetBool("walking", true);
+                anim.SetInteger("condition", 3);
+                speed = RunSpeed;
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetBool("walking", false);
+                anim.SetInteger("condition", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                anim.SetBool("running", false);
+                speed = NormalSpeed;
+            }
+            
+
+
+        }
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+
+    void GetInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (anim.GetBool("walking") == true)
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("condition", 0);
+                }
+                if (anim.GetBool("walking") == false)
+                {
+                    Attacking();
+                }
+                
+            }
+        }
+    }
+
+    void Attacking()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        anim.SetBool("attacking", true);
+        anim.SetInteger("condition", 2);
+        yield return new WaitForSeconds(1);
+        anim.SetInteger("condition", 0);
+        anim.SetBool("attacking", false);
+    }
+}
