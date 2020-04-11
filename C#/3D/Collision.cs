ed6e4ca1a18f7668551bdbc839015cde799ad283
Stefan <bitupgrade@outlook.com>
2020-04-11 17:31:09 +0100

    added some new scripts

---
+++ C#/3D/Collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    public float speed = 6f;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        velocity = direction * speed;
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.position += velocity * Time.deltaTime;
+    }
+}
